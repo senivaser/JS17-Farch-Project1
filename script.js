a88235960b771c7d72f28a97fa0be9e0da965888
Ivan Seyushkin <senivaser@gmail.com>
2021-01-28 10:09:40 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+//Инициализация переменных (1.3)
+
+let money
+let income 
+let addExpenses
+let deposit
+let mission
+let period
+
+//Вывод на экран с помощью alert() (1.4)
+
+alert('Hello, world');
+
+
