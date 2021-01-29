7caab07be2f9ba60fa76d4227607c4246167541e
Ivan Seyushkin <senivaser@gmail.com>
2021-01-29 13:05:30 +0300

    c02

--- script.js
+++ script.js
@@ -11,21 +11,21 @@ period = 6;
 
 
 //(2.1.2)
-console.log('--------2.1---------')
+console.log('--------2.1---------');
 console.log('Types of variables:');
 console.log('money: ', typeof(money));
 console.log('income: ', typeof(income));
 console.log('deposit: ', typeof(deposit));
-console.log('-----------------------')
-console.log('Length of addExpences: ', addExpenses.length)
-console.log('-----------------------')
-console.log(`Период равен ${period} месяцев`)
-console.log(`Цель заработать ${mission} рублей`)
-console.log('-----------------------')
-console.log(addExpenses.toLowerCase().split(''))
+console.log('-----------------------');
+console.log('Length of addExpences: ', addExpenses.length);
+console.log('-----------------------');
+console.log(`Период равен ${period} месяцев`);
+console.log(`Цель заработать ${mission} рублей`);
+console.log('-----------------------');
+console.log(addExpenses.toLowerCase().split(''));
 
-let budgetDay = 2000
-console.log('Дневной бюджет: ', budgetDay)
+let budgetDay = 2000;
+console.log('Дневной бюджет: ', budgetDay);
 
 
 // //Вывод на экран с помощью alert() (1.4)
