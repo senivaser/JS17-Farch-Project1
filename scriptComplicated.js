be49eec0b1f9be5ac4829b43fb0ae2e387eb0128
Ivan Seyushkin <senivaser@gmail.com>
2021-02-02 23:04:38 +0300

    cl04c02

--- scriptComplicated.js
+++ scriptComplicated.js
@@ -1,9 +1,9 @@
 'use-strict'
 
 const strModeration = function (str) {
-  const newStr = str.slice(0, 30).split('')
-  const newStrReverse = [].concat(newStr).reverse()
   if (typeof(str) !== 'string') return 'Wrong type of argument';
+  const newStr = str.slice(0, 30).split('');
+  const newStrReverse = [].concat(newStr).reverse();
   return newStr
   .slice(newStr.findIndex(c => c !== ' '), newStr.length - newStrReverse.findIndex(c => c !== ' '))
   .join('')
@@ -14,3 +14,4 @@ const strModeration = function (str) {
 console.log(strModeration('     Мама  мыла  раму     '))
 console.log(strModeration('     Мама мыла раму                            '))
 console.log(strModeration('     Мама мыла раму                        12333'))
+console.log(strModeration(1))
