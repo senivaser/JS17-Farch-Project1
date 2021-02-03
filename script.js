fbd38822ea476f21e127a994eb5e9320e66ff2da
Ivan Seyushkin <senivaser@gmail.com>
2021-02-03 21:20:01 +0300

    l05c01

--- script.js
+++ script.js
@@ -23,60 +23,96 @@ const getDaysInNextMonth = function() {
 
 //Ввод входных данных
 
-money = prompt('Ваш Месячный доход? (р)'); //3.1.2
+//5.1.1
+const start = function () {
+  money = 0;
+  do {
+    money = prompt('Ваш месячный доход');
+  } while (isNaN(parseFloat(money)));
+  
+}
 
+start(); //Сорри, что функция не чистая, но так в уроке написано. =( 
+
+//3.1
 addExpenses = prompt(`Перечислите возможные расходы за расчитываемый\
   период через запятую (пример: Квартплата, проездной, кредит)`)
+  .toLowerCase()
   .split(',')
   .map(item => item.replace(/\s/g, ''));     //3.1.3
 
 
 deposit = confirm(`Есть ли у вас депозит в банке?`); //3.1.4
 
-//3.1.5
-let expenses1 = prompt('Введите обязательную статью расходов (1):');
-let amount1 = +prompt(`Во сколько вам обойдется статья расходов \"${expenses1}\"? (р)`);
-let expenses2 = prompt('Введите обязательную статью расходов (2):');
-let amount2 = +prompt(`Во сколько вам обойдется статья расходов \"${expenses2}\"? (р)`);
+//Вспомогательные промты 5.1
+const checkNumberPromt = function (message, exception='') {
+  let result = 0;
+  do {
+    if (isNaN(result)) {
+      console.log(`Entry is not a number: try again`);
+      exception = '(Вы не ввели число. Повторите ввод, пожалуйста)';
+    }
+    result = +prompt(message + exception);
+  } while (isNaN(result));
 
-let expenses = {
-  expenses1: amount1,
-  expenses2: amount2
-}
+  return result;
 
-//4.1.1
-const getExpensesMonth = function (amount1, amount2) {
+} 
+
+
+const checkNaturalPromt = function (message, checkNumberPromt, exception = '') {
+   const result = checkNumberPromt(message, exception);
+   if (parseFloat(result) < 1) {
+     return checkNaturalPromt(message, checkNumberPromt, 'Вы ввели ненатуральное число. Повторите ввод, пожалуйста');
+   } else return result;
   
-  return amount1 + amount2;
-
 }
 
 
-//4.1.2
-const getAccumulatedMonth = function (money, amount1, amount2, getExpensesMonthFunction) {
+//5.1.2
+let expenses = []
+
+const getExpensesMonth = function () {
   
-  return money - getExpensesMonthFunction(amount1, amount2);
+  let sum = 0
+  const numOfExpenses = checkNaturalPromt('Введите количество статей расхода: ', checkNumberPromt);
+  for (let i = 0; i < numOfExpenses; i++) {
+    expenses[i] = prompt('Введте обязательную статью расходов'); // тож сорри
+    sum += checkNumberPromt('Во сколько это обойдется');
+  }
+  
+  return sum ;
 
 }
 
-//4.1.3
-let accumulatedMonth = getAccumulatedMonth(money, amount1, amount2, getExpensesMonth);
+let expensesSum = getExpensesMonth();
 
-//4.1.4
+//4.1.2 reworked 5.1
+const getAccumulatedMonth = function (money, expensesSum) {
+  
+  return money - expensesSum;
+
+}
+
+//4.1.3 reworked 5.1
+let accumulatedMonth = getAccumulatedMonth(money, expensesSum);
+
+//4.1.4 reworked 5.1.3
 const getTargetMonth = function (mission, accumulatedMonth) {
   
-  return (accumulatedMonth >= 0) ?
-    Math.ceil(mission/accumulatedMonth): 
-    'Неопределенное количество';
+  return Math.ceil(mission/accumulatedMonth); 
+
 
 }
+
+let targetMonth = getTargetMonth(mission, accumulatedMonth);
 
 //4.1.6
 budgetDay = accumulatedMonth/getDaysInNextMonth();
 
 //4.1.7
 
-//Новые Функции
+//Новые Функции 4.1
 
 const showTypeOf = function (data) {
   
@@ -107,17 +143,18 @@ console.log('------');
 
 //b
 console.log('---getExpensesMonth---');
-console.log(getExpensesMonth());
+console.log(expensesSum);
 console.log('------');
 
 //c
-console.log('---addExpences---');
+console.log('---addExpenses---');
 console.log(addExpenses);
 console.log('------');
 
-//d
+//d 
 console.log('---getTargetMonth---');
-console.log(`Время достижения цели (${mission} р.): ${getTargetMonth()} месяцев(-а)`, );
+console.log(`Цель (${mission} р.)\
+ ${targetMonth > 0 ? `будет достигнута через ${targetMonth} месяцев(-а)`: 'не будет достигнута'}`);
 console.log('------');
 
 //e
@@ -127,5 +164,5 @@ console.log('------');
 
 //e
 console.log('---showStatusIncome---');
-console.log(showStatusIncome());
+console.log(showStatusIncome(budgetDay));
 console.log('------');
