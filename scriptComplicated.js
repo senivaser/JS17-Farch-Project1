af1b2d434921e72b25954c01b8f22f29cd348313
Ivan Seyushkin <senivaser@gmail.com>
2021-02-03 21:44:41 +0300

    cl05c01

--- scriptComplicated.js
+++ scriptComplicated.js
@@ -1,17 +1,17 @@
 'use-strict'
 
-const strModeration = function (str) {
-  if (typeof(str) !== 'string') return 'Wrong type of argument';
-  const newStr = str.slice(0, 30).split('');
-  const newStrReverse = [].concat(newStr).reverse();
-  return newStr
-  .slice(newStr.findIndex(c => c !== ' '), newStr.length - newStrReverse.findIndex(c => c !== ' '))
-  .join('')
-  .concat(str.length > 30 ? '...': '')
+//5.2.1
+const task1 = function(){
+    const arr =  ['123123', '123123', '123123', '23232' ,'434234', '2342342', '312313'];
+    arr.map(item => {
+        if (item.slice(0) === '2' || item.slice(0) === '4') console.log(item);
+    })
+}
 
-} 
-
-console.log(strModeration('     Мама  мыла  раму     '))
-console.log(strModeration('     Мама мыла раму                            '))
-console.log(strModeration('     Мама мыла раму                        12333'))
-console.log(strModeration(1))
+//5.2.2
+const task2 = function(){
+    const simpleNumbers = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
+    for (let i = 0; i < simpleNumbers.length - 1; i++) {
+        console.log(simpleNumbers[i],  ` Делители этого числа: 1, ${simpleNumbers[i]}`);
+    }
+}
