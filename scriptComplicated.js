bc93a62a69d605631a7d2bcd002dfff87f09b422
Ivan Seyushkin <senivaser@gmail.com>
2021-02-06 16:30:38 +0300

    cl07c01

--- scriptComplicated.js
+++ scriptComplicated.js
@@ -1,30 +1,27 @@
 'use-strict'
 
-//5.2.1
-const task1 = function(){
-    const arr =  ['123123', '123123', '123123', '23232' ,'434234', '2342342', '312313'];
-    arr.map(item => {
-        if (item.slice(0,1) === '2' || item.slice(0,1) === '4') console.log(item);
-    })
+const getCurrentDay = function() {
+    return new Date().getDay()
 }
 
-//5.2.2
+const currentDay = getCurrentDay()
 
-const findSimpleNumbers = function (border) {
-    let arr = Array.from({length: border}, (e, i) => i + 2);
-    for (let i = 2; i < border; i++) {
-        if (arr.find((item) => item === i)) {
-            arr = arr.filter(item => ((item%i !== 0) || (item === i)))
-        }
-    }
-    return arr
-}
+const root = document.getElementById('root')
 
-const task2 = function(){
-    const simpleNumbers = findSimpleNumbers(1000)
-    for (let i = 0; i < simpleNumbers.length - 1; i++) {
-        console.log(simpleNumbers[i],  ` Делители этого числа: 1, ${simpleNumbers[i]}`);
-    }
-}
+const week = ["Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday"]
 
-task2();
+week.forEach((dayName, numberOfDay) => {
+    const bold = (getCurrentDay() === numberOfDay) ? 'bold': ''
+    const italic = (numberOfDay > 4)? 'italic': '';
+    const dayNode = document.createElement('div')
+    dayNode.setAttribute('class', `day ${bold} ${italic}`)
+    dayNode.innerText = dayName
+    root.appendChild(dayNode)
+})
+
