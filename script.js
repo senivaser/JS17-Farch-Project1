eb3a9d24b43b27af722eef34261ee269e8be40be
Ivan Seyushkin <senivaser@gmail.com>
2021-02-06 15:56:07 +0300

    l07c01

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 'use strict'
 
-////////////Вспомогательные функции///////////////
+//#region Вспомогательные функции
+
 //Проверка на тип "Число" из потокового ввода
 function isNumber (n){
     return !isNaN(parseFloat(n)) && isFinite(n);
@@ -18,18 +19,102 @@ Date.prototype.daysInNextMonth = function() {
     return new Date().daysInNextMonth()
   }
 
+//#region checkPromt
+
+/* 
+
+                    checkPromt() - расширенный promt
+
+Возвращает результат корректного ввода, если иного не подразумевают аргументы.
+
+Может вызываться с различными аргументами в порядке очереди:
+
+message - основное сообщение; 
+
+conditions - определяет корректность ввода, является упорядоченным списком из элементов 
+    ["сообщение", условие вида (результат ввода)=> (условие(результат ввода))], в случае 
+    правдивого условия повторяет запрос с доп выводом "сообщения";
+
+isNumPromt - если true, то promt работает как +promt, однако значение null не изменяется; 
+
+isNullPass - результат ввода null проходит все проверки; 
+
+isEmptyPass - если на входе пустая строка, результат ввода считается null
+
+isExitReturn - если true, то в случае первого правдивого условия из conditions не повторяет
+    запрос, а возвращает ["сообщение",  результат ввода]; 
+
+addMessage - доп сообщение для первого запроса
 
 
-////////////Основная программа///////////////
+*/
+
+const checkConditions = function(conditions, trigger, result, addMessage){
+    const checkResult = Boolean(conditions && conditions[0][1](result));
+    
+    if (checkResult) {
+        trigger = true;
+        addMessage = conditions && conditions[0][0];
+    }
+    
+    conditions && conditions.shift()
+    
+    return (trigger || !conditions.length) ? 
+        [trigger, addMessage]: 
+        checkConditions(conditions, trigger, result, addMessage);
+}
+
+const checkPromt = function (
+    message = '', 
+    conditions = [],
+    isNumPromt = false,
+    isEmptyPass = false,
+    isNullPass = false,
+    isExitReturn = false,
+    addMessage = '') {
+    
+    
+        let localConditions = [].concat(conditions);
+        let trigger = false;
+        let result = prompt(`${message} ${(addMessage !== '') ? `(${addMessage})`: ''}`);
+        result = (isEmptyPass) ? (result.trim() === '') ? null: result: result;
+        result = (result === null) ? null: (isNumPromt) ? +result: result;
+
+        [trigger, addMessage] = checkConditions(localConditions, trigger, result, addMessage)
+        
+        if (result === null && isNullPass) trigger = false;
+
+        if (isExitReturn) {
+            result = [addMessage, result];
+            trigger = false;
+        }
+
+        return (trigger) ? checkPromt(message, 
+            conditions,
+            isNumPromt,
+            isEmptyPass,
+            isNullPass,
+            isExitReturn,
+            addMessage) :
+            result
+
+}
+
+//#endregion checkPromt
+
+//#endregion Вспомогательные функции
+
+
+//#region Основная программа
 
 //Старт программы. Ввод дохода за месяц
-let money = 10000
+let money;
 
 const start = function () {
     money = 0;
     do {
-      money = +prompt('Ваш месячный доход');
-    } while (isNumber(money));
+        money = +prompt('Ваш месячный доход');
+    } while (!isNumber(money));
     
 }
 
@@ -45,138 +130,142 @@ const appData = {
     deposit: false,
     mission: 50000,
     period: 3,
+    budget: money,             //7.1.2
+    budgetDay: 0,              //7.1.3
+    budgetMonth: 0,            //7.1.3
+    expensesMonth: 0,          //7.1.3
     asking: function(){
         
         let addExpenses = prompt(`Перечислите возможные расходы за расчитываемый\
         период через запятую (пример: Квартплата, проездной, кредит)`)
 
-        appData.addExpenses = addExpenses
+        appData.addExpenses = (addExpenses) ?  
+            addExpenses
             .toLowerCase()
             .split(',')
-            .map(item => item.trim()); 
+            .map(item => item.trim()) :
+            []; 
 
         appData.deposit = confirm(`Есть ли у вас депозит в банке?`);
         
+    },
+
+    //#region 7.1.4 getMethods
+
+    //7.1.8
+    getExpensesMonth: function () {
+
+        appData.expensesMonth = 0;
+        
+        const numOfExpenses = checkPromt('Введите количество статей расхода: ',
+            [
+                ['Ввод не может быть пустым', (result) => (result === null)],
+                ['Это не число', (result) => (!isNumber(result))],
+                ['Число не натуральное. Повторите ввод', (result) => (!(Number.isInteger(result) && result > 0))]
+            ],
+            true, true            
+            );
+        
+        for (let i = 0; i < numOfExpenses; i++) {
+
+            let expenseLabel = checkPromt(`Введите обязательную статью расходов (${i+1})`,
+            [
+                ['Ввод не может быть пустым', (result) => (result === null)],
+                ['Данная статья расходов уже присутствует в списке', 
+                    (result) => (Object.keys(appData.expenses).find((target => target === result)))]
+            ],
+            false, true
+            );
+
+            let expenseValue = checkPromt(`Во сколько это обойдется`,
+            [   
+                ['Ввод не может быть пустым', (result) => (result === null)],
+                ['Это не число', (result) => (!isNumber(result))],
+                ['Расходы не могут иметь отрицательное значение', (result) => (result<0)]
+            ],
+            true, true            
+            );
+            
+            appData.expenses = {...appData.expenses, [expenseLabel]: expenseValue};
+        }
+        
+        for (let label in appData.expenses) {
+            appData.expensesMonth += appData.expenses[label]
+        }
+        
+        return appData.expensesMonth
+    },    
+    
+    //7.1.9
+    getBudjet: function () {
+
+        appData.budgetMonth = appData.budget - appData.expensesMonth;
+        appData.budgetDay = appData.budgetMonth/getDaysInNextMonth();
+  
+        return [appData.budgetMonth, appData.budgetDay]
+      
+    },    
+    
+    //7.1.10
+    getTargetMonth: function () {
+  
+        return Math.ceil(appData.mission/appData.budgetMonth); 
+         
+    },
+    getStatusIncome: function() {
+
+        if (!isNaN(appData.budgetDay)) {
+            if (appData.budgetDay >= 0 && appData.budgetDay < 600) return 'К сожалению, ваш уровень дохода ниже среднего';
+            else if (appData.budgetDay >= 600 && appData.budgetDay < 1200) return 'К сожалению, ваш уровень дохода ниже среднего';
+            else if (appData.budgetDay >= 1200) return 'К сожалению, ваш уровень дохода ниже среднего';
+            else return 'К сожалению, ваш уровень дохода ниже среднего';
+        }
+        else return 'К сожалению, ваш уровень дохода ниже среднего';
+      
     }
-
+    //#endregion 7.1.4
 }
+//#region 7.1.6 Вызов функции ввода данных
+appData.asking()   
+//#region 7.1.6
+
+//#region 7.1.5 || 7.1.11 Вызов функций изменения appData
+appData.getExpensesMonth()
+
+appData.getBudjet();
+
+let targetMonth = appData.getTargetMonth();
+
+let statusIncome = appData.getStatusIncome()
+//#endregion 7.1.5 || 7.1.11
+
+//#endregion Основная программа
 
 
+//#region 7.1.12 || 7.1.13 Вывод 
 
-//Вспомогательные промты 5.1
-const checkNumberPromt = function (message, exception='') {
-  let result;
-  do {
-    result = +prompt(message + exception);
-    if (isNaN(result) || typeof(result) !== 'number') {
-      console.log('Entry is not a number: try again');
-      exception = '(Вы не ввели число. Повторите ввод, пожалуйста)';
-    }
-  } while (isNaN(result) || typeof(result) !== 'number');
-
-  return result;
-
-} 
-
-
-const checkNaturalPromt = function (message, checkNumberPromt, exception = '') {
-   const result = checkNumberPromt(message, exception);
-   if (parseFloat(result) < 1) {
-     return checkNaturalPromt(message, checkNumberPromt, 'Вы ввели ненатуральное число. Повторите ввод, пожалуйста');
-   } else return result;
-  
-}
-
-
-let expenses = []
-
-const getExpensesMonth = function () {
-  
-  let sum = 0
-  const numOfExpenses = checkNaturalPromt('Введите количество статей расхода: ', checkNumberPromt);
-  for (let i = 0; i < numOfExpenses; i++) {
-    expenses[i] = prompt('Введте обязательную статью расходов'); // тож сорри
-    sum += checkNumberPromt('Во сколько это обойдется');
-  }
-  
-  return sum ;
-
-}
-
-let expensesSum = getExpensesMonth();
-
-
-
-//4.1.2 reworked 5.1
-const getAccumulatedMonth = function (money, expensesSum) {
-  
-  return money - expensesSum;
-
-}
-
-
-
-//4.1.3 reworked 5.1
-let accumulatedMonth = getAccumulatedMonth(money, expensesSum);
-
-
-
-//4.1.4 reworked 5.1.3
-const getTargetMonth = function (mission, accumulatedMonth) {
-  
-  return Math.ceil(mission/accumulatedMonth); 
-
-
-}
-
-let targetMonth = getTargetMonth(mission, accumulatedMonth);
-
-
-
-//4.1.6
-
-let budgetDay = accumulatedMonth/getDaysInNextMonth();
-
-//4.1.7
-
-//Новые Функции 4.1
-
-const showStatusIncome = function(budgetDay) {
-
-  if (!isNaN(budgetDay)) {
-    if (budgetDay >= 0 && budgetDay < 600) return 'К сожалению, ваш уровень дохода ниже среднего';
-    else if (budgetDay >= 600 && budgetDay < 1200) return 'К сожалению, ваш уровень дохода ниже среднего';
-    else if (budgetDay >= 1200) return 'К сожалению, ваш уровень дохода ниже среднего';
-    else return 'К сожалению, ваш уровень дохода ниже среднего';
-  }
-  else return 'К сожалению, ваш уровень дохода ниже среднего';
-
-} 
-
-//Вывод
-
-//b
+//7.1.12
+//a
 console.log('---getExpensesMonth---');
-console.log(expensesSum);
+console.log(appData.expensesMonth, appData.expenses);
+appData.expenses
 console.log('------');
 
-//c
-console.log('---addExpenses---');
-console.log(addExpenses);
-console.log('------');
-
-//d 
+//b 
 console.log('---getTargetMonth---');
-console.log(`Цель (${mission} р.)\
+console.log(`Цель (${appData.mission} р.)\
  ${targetMonth > 0 ? `будет достигнута через ${targetMonth} месяцев(-а)`: 'не будет достигнута'}`);
 console.log('------');
 
-//e
-console.log('---budgetDay---');
-console.log(`Ваш бюджет на день: ${Math.floor(budgetDay)} р.`);
+//c
+console.log('---showStatusIncome---');
+console.log(statusIncome);
 console.log('------');
 
-//e
-console.log('---showStatusIncome---');
-console.log(showStatusIncome(budgetDay));
-console.log('------');
+//7.1.13
+console.log("Наша программа включает в себя данные: ")
+for (let property in appData) {
+    console.log(property)
+}
+
+//#endregion 7.1.12  Вывод
