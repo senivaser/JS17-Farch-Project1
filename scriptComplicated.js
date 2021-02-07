c5b190184e39ff7b7559b287f1e7a816f4821de4
Ivan Seyushkin <senivaser@gmail.com>
2021-02-07 13:43:26 +0300

    cl07c02

--- scriptComplicated.js
+++ scriptComplicated.js
@@ -1,27 +1,30 @@
 'use-strict'
 
 const getCurrentDay = function() {
-    return new Date().getDay()
+    return new Date().getDay();
 }
 
-const currentDay = getCurrentDay()
+const currentDay = getCurrentDay();
 
-const root = document.getElementById('root')
+const root = document.getElementById('root');
 
-const week = ["Monday",
+const week = [    
+    "Monday",
     "Tuesday",
     "Wednesday",
     "Thursday",
     "Friday",
     "Saturday",
-    "Sunday"]
+    "Sunday"
+];
 
 week.forEach((dayName, numberOfDay) => {
-    const bold = (getCurrentDay() === numberOfDay) ? 'bold': ''
+    const bold = ((getCurrentDay()+6)%7 === numberOfDay) ? 'bold': '';
     const italic = (numberOfDay > 4)? 'italic': '';
-    const dayNode = document.createElement('div')
-    dayNode.setAttribute('class', `day ${bold} ${italic}`)
-    dayNode.innerText = dayName
-    root.appendChild(dayNode)
+    const dayNode = document.createElement('div');
+    dayNode.setAttribute('class', `day ${bold} ${italic}`);
+    dayNode.innerText = dayName;
+    root.appendChild(dayNode);
 })
+
 
