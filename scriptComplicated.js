38dd67487a2cf7212d1d36e3385ea5ec1e497f63
Ivan Seyushkin <senivaser@gmail.com>
2021-02-15 17:54:45 +0300

    cl13c01

--- scriptComplicated.js
+++ scriptComplicated.js
@@ -1,30 +1,53 @@
 'use-strict'
 
-const getCurrentDay = function() {
-    return new Date().getDay();
+//Рандомное число от 0 до 255 (двухразрядных в 16-ричной системе)
+const generate2DigitHex = () => {
+    const decNumber = Math.floor(Math.random()*256);
+    return (decNumber === 256) ? 
+        generate2DigitHex():
+        decNumber;
 }
 
-const currentDay = getCurrentDay();
+//Новый цвет в виде массиваиз 3-х чисел (RGB) в десятичной системе
+const createNewColor = () => {
+    return [
+        generate2DigitHex(),
+        generate2DigitHex(),
+        generate2DigitHex()
+    ];
+}
 
-const root = document.getElementById('root');
-
-const week = [    
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-    "Sunday"
-];
-
-week.forEach((dayName, numberOfDay) => {
-    const bold = ((getCurrentDay()+6)%7 === numberOfDay) ? 'bold': '';
-    const italic = (numberOfDay > 4)? 'italic': '';
-    const dayNode = document.createElement('div');
-    dayNode.setAttribute('class', `day ${bold} ${italic}`);
-    dayNode.innerText = dayName;
-    root.appendChild(dayNode);
-})
+//Преобразование цвета из createNewColor в строку hex
+const convertToHexColor = (arr) => {
+    return arr.reduce((color, item) => color + ((item > 15) ? item.toString(16): '0' + item.toString(16)), '#');
+} 
 
 
+//Определяется цвет надписи по сумме квадратов отклонений RGB от 255
+const BlackOrWhite = (colorDec) => {
+    
+    const whiteRange = colorDec.reduce((sum, item) => sum + (256-item)**2, 0)
+    return ( whiteRange > 50000) ? 'white': 'black';
+
+}
+
+const rootN = document.querySelector('#root');
+const colorStringN = document.querySelector('.colorString');
+const colorButtonN = document.querySelector('.colorButton');
+const labelN = document.querySelector('.label');
+
+const render = () => {
+    const decColor = createNewColor();
+    const color = convertToHexColor(decColor);
+    const bORw = BlackOrWhite(decColor);
+    
+    colorStringN.innerText = `${color}`;
+    rootN.setAttribute('style', `background-color: ${color}`);
+    colorButtonN.setAttribute('style', `background-color: ${bORw}`);
+    colorStringN.setAttribute('style', `color: ${bORw}`);
+    labelN.setAttribute('style', `color: ${color}`)    
+}
+
+colorButtonN.addEventListener('click', render)
+
+render()
