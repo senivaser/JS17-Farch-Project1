6c1dfa8b95b2f35508de28bffc86b0fcdba551e9
Ivan Seyushkin <senivaser@gmail.com>
2021-02-17 18:32:08 +0300

    l15pc01

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 'use-strict'
+
+class First{
+    constructor(){
+
+    }
+    hello() {
+        console.log('Привет, я метод родителя!')
+    }
+}
+
+class Second extends First {
+    constructor(){
+        super()
+    }
+    hello() {
+        super.hello()
+        console.log('А я наследуемый метод!')
+    }
+}
+
+const fObj = new First()
+const sObj = new Second()
+
+fObj.hello()
+console.log('-----------------')
+sObj.hello()
