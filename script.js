6adc7ff7d08dfd33edcadfdc3fea0144e798b02c
Ivan Seyushkin <senivaser@gmail.com>
2021-02-17 18:22:57 +0300

    l15pc00

--- script.js
+++ script.js
@@ -1,15 +1,1 @@
-//Инициализация переменных (1.3)
-
-let money
-let income 
-let addExpenses
-let deposit
-let mission
-let period
-
-//Вывод на экран с помощью alert() (1.4)
-
-alert('Hello, world');
-console.log('Hello world!')
-console.log('Hello JS!')
-
+'use-strict'
