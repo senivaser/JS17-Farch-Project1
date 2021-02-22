3d30041d1190594000766b7a11caabacdc29fd8c
Ivan Seyushkin <senivaser@gmail.com>
2021-02-22 18:33:21 +0300

    cl18bc00

--- script.js
+++ script.js
@@ -1,20 +1,2 @@
 'use-strict'
 
-const intputN = document.querySelector('#input')
-
-const resulnN = document.querySelector('#result')
-
-let timeout = null
-
-const showValue = () => {
-
-  resulnN.innerText = intputN.value
-  
-}
-
-intputN.addEventListener('input', (event) => {
-  
-  if (timeout) clearTimeout(timeout)
-  timeout = setTimeout(showValue, 300)
-
-})
